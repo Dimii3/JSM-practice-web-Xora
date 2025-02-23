c9b912668a603cb94378e9edfc26bc4ffb171782
Dawid <dawid.marchewczyk@gmail.com>
2025-02-23 19:58:25 +0100

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Header from "./sections/Header";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world! 🔥</h1>;
+  return (
+    <main>
+      <Header></Header>
+    </main>
+  );
 }
