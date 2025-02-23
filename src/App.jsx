4f721773ec9b5533bbf3ad95b88ebe9f48a58f6c
Dawid <dawid.marchewczyk@gmail.com>
2025-02-23 23:40:39 +0100

    create features-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
+import Features from "./sections/Features";
 
 export default function App() {
   return (
     <main>
       <Header></Header>
       <Hero></Hero>
+      <Features></Features>
     </main>
   );
 }
