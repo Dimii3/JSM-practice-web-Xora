51c4df4aa05b3cea9fa8ab52ef2d1db8938b11bd
Dawid <dawid.marchewczyk@gmail.com>
2025-02-24 21:23:12 +0100

    create faq-section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
+import Faq from "./sections/Faq";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Hero></Hero>
       <Features></Features>
       <Pricing></Pricing>
+      <Faq></Faq>
     </main>
   );
 }
